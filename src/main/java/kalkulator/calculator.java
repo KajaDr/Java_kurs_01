51ce6e35884f7a2ee2a0eb395a16b388a3d5d358
Kaja <k.drozdz94@gmail.com>
2022-07-13 16:46:55 +0200

    pierwszy commit

---
+++ src/main/java/kalkulator/calculator.java
@@ -0,0 +1,10 @@
+package kalkulator;
+
+public class calculator {
+    public static void result(){
+        System.out.println("Result of adding" +);
+        System.out.println("Result of multiplication" +);
+        System.out.println("Result of substraction" +);
+        System.out.println("Result of dividing by 0" +);
+    }
+}
---
+++ src/main/java/pl/cyber/trainees/przyklady_klas/JakDzialajaKlasy.java
@@ -0,0 +1,29 @@
+package pl.cyber.trainees.przyklady_klas;
+
+import java.math.BigDecimal;
+
+public class JakDzialajaKlasy {   // tu zaczyna się klasa
+
+    private String SPACJA = " ";  // parametr klasy
+
+    public String nazwa_metody(String parametr) { // tu zaczyna się klasa nr 1
+        //Metoda coś wykonuje
+
+        return "zwracamy napis";
+    }// tu kończy się klasa nr 1
+
+    private BigDecimal nazwa_metody2(BigDecimal parametr) {// tu zaczyna się klasa nr 2
+        //Metoda coś wykonuje
+//    return BigDecimal.ONE;
+
+        String liczba1 = "1";
+        String liczba2 = "12";
+
+
+        return new BigDecimal(liczba1 + liczba2); // 112
+    }// tu kończy się klasa nr 1
+
+
+
+} // tu kończy się nasza klasa
+
