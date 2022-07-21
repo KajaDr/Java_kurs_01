e50da1e2e26b21a0889909d2ae176d8a70bc2e05
Kaja <k.drozdz94@gmail.com>
2022-07-21 02:44:44 +0200

    Spotkanie_16072022_Praca domowa_Czesc_03 ( dodatkowa)

--- src/main/java/pl/cyber/trainees/PracaDomowa_03/CarApp2.java
+++ src/main/java/pl/cyber/trainees/PracaDomowa_03/CarApp2.java
@@ -3,34 +3,36 @@ package pl.cyber.trainees.PracaDomowa_03;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CarApp2 {
     public static void main(String[] args) throws FileNotFoundException {
 // podczytanie plików
         Cars readData2 = new Cars();
-        String car01 = readData2.DateFromFile(new File("src/main/resources/samochody/pojazd_1"));
-        String car02= readData2.DateFromFile(new File("src/main/resources/samochody/pojazd_2"));
-        String car03 = readData2.DateFromFile(new File("src/main/resources/samochody/pojazd_3"));
+        String car01 = "\ncar 01: " + readData2.DateFromFile(new File("src/main/resources/samochody/pojazd_1"));
+        String car02= "\ncar 02: " + readData2.DateFromFile(new File("src/main/resources/samochody/pojazd_2"));
+        String car03 = "\ncar 03: " + readData2.DateFromFile(new File("src/main/resources/samochody/pojazd_3"));
 
-        System.out.println(car01);
 //stworzenie listy z samochodów
-        List<String> cars = new ArrayList<>();
+        List<String> cars = new LinkedList<>();
         cars.add(car01);
         cars.add(car02);
         cars.add(car03);
 
-        class LoopCar {
+//pętla
             LoopCar loopCar= new LoopCar();
             for( String newCar: cars){
 
                     if (newCar == car01){
-                        System.out.println("car: " + car01);
+                        System.out.println("All Cars: " + cars);
                     } else {
+                        System.out.println("");
 
                     }
                 }
             }
 
-        }
+    private static class LoopCar {
     }
+}
