b03d382f3529c90cfbf0d08807af9530cfdd3d12
Kaja <k.drozdz94@gmail.com>
2022-07-22 20:08:28 +0200

    czyszczenie Simpple APP

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -1,5 +1,13 @@
-package pl.cyber.trainees;
+//1. Konstrukcja klas i metod
+package pl.cyber.trainees; // okreslenie pakietu gdzie znajduje sie klasa
+//packege robi sie automatycznie, jesli plik zawiera sie w odpowiedniej strukturze
+//katalog tj src/main/java/...
+//region miejsce na importy
+//import java.io.File;
+import java.math.*;  // z * mówimy że chcemy zaimportować wszystkie biblioteki wramach pakietu (package) w tym przypadku java.math
+//endregion
 
+//endregion
 
 import pl.cyber.trainees.dziedziczenie.Kolor;
 import pl.cyber.trainees.dziedziczenie.Model;
