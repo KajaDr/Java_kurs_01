804f9c4693348ff9e60e9daaa5ba25bd58f7e125
Kaja <k.drozdz94@gmail.com>
2022-07-22 19:53:18 +0200

    czyszczenie Simpple APP

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -8,16 +8,6 @@ public class SimpleApp {
 
 // Ctrl +P podpowiadani w użyciu metod/deklaracji obiektów dostępnch parametrów
     public static void main(String[] args) {
-         Model modelPojazdu1 = new Model();
-        var modelPojazdu2 = new Model("VW", "2020", "przód","Pasat",10000, 2.0, Kolor.ZIELONY);
-        var modelPojazdu3 = new Model("BMW", "2021", "tył","faae",20000, 2.0, Kolor.CZERWONY);
-
-        System.out.println(modelPojazdu2.toString());
-
-        System.out.println(modelPojazdu2.getMarka());
-        System.out.println(modelPojazdu2.getNaped());
-        System.out.println(modelPojazdu2.getNazwa());
-//można kolor wpisać z metody Valueoff
 
     }
 
